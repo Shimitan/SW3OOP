6516e535d83e4954b9a77f0e4a81214850ebce09
Shimitan <43805784+Shimitan@users.noreply.github.com>
2021-09-10 22:42:08 +0200

    AAA

---
+++ Lecture1/printNestedLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace printNestedLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int j = 0; j <= 5; j++)
+            {
+                Console.WriteLine(" ");
+                for (int i = 0; i <= j; i++)
+                {
+                    Console.Write("*");
+                }
+            }
+        }
+    }
+}
