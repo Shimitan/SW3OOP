01f2bd3acfb315c6dd776d99e6220fad1713c152
Mike Jensen <43805784+Shimitan@users.noreply.github.com>
2021-10-15 11:44:15 +0200

    Lecture5 exercise1

---
+++ Exercises/Lecture5/BankAccountNonnegativeBalance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankAccountNonnegativeBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Exercises/Lecture5/ExceptionHandlingIntegerInput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExceptionHandlingIntegerInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            
+            Console.WriteLine("Please input an integer: ");
+            string intnumber = Console.ReadLine();
+
+            try
+            {
+                Int32.Parse(intnumber);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Your input was not an integer");
+                throw;
+            }
+            Console.WriteLine("Your inputted integer is: " + intnumber);
+        }
+    }
+}
