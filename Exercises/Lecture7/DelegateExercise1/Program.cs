5afc8cc0db9078ede297edbac3f8bef30d3d9413
Mike Jensen <43805784+Shimitan@users.noreply.github.com>
2021-10-29 12:42:25 +0200

    Exercise1

--- Exercises/Lecture7/DelegateExercise1/Program.cs
+++ Exercises/Lecture7/DelegateExercise1/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Threading.Channels;
 
 namespace DelegateExercise1
 {
     class Program
     {
+        private delegate string Join(string message1, string message2);
+
+        private static string DelegateMethod(string message1, string message2, Join join)
+        {
+            return join(message1, message2);
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string message = DelegateMethod("Hello ", "World!", ((message1, message2) => message1 + "." + message2));
+            Console.WriteLine(message);
         }
     }
 }
---
+++ Exercises/Lecture7/DelegateExercise2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DelegateExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private delegate string Join(string message1, string message2);
+
+            private static string StringJoin(string message1, string message2, Join join)
+            {
+                return join(message1, message2);
+            }
+
+            private static string JoinThree(string message1, string message2, string message3, Join join)
+            {
+                string jointwo = join(message1, message2);
+                return (join( message3));
+            }
+            
+            static void Main(string[] args)
+            {
+                string message = StringJoin("Hello ", "World!", " Bob" ((message1, message2, message3) => message1 + message2, message3));
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
